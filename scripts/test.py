821c5b1e70803c0000624ac39ef4213f7ebec7fc
Paul Champion <paul.champion92@gmail.com>
2020-02-07 15:07:58 +0100

    Finalize EmbeddingLoader 300 features, transfer pickle file

--- scripts/test.py
+++
@@ -1,9 +0,0 @@
-import pickle
-from torch.nn import Embedding
-with open("embedding_dic.pickle","rb") as f:
-    weights=pickle.load(f)
-
-import ipdb; ipdb.set_trace()
-print(type(data))
-
-em=Embedding(*data)
